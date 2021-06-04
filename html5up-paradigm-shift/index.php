c5a53e243d0df92d2367d0b3281a46ecc4934ea4
sangchu <sangchu9704@gmail.com>
2021-06-04 18:11:41 +0900

    최종수정함

--- html5up-paradigm-shift/index.php
+++ html5up-paradigm-shift/index.php
@@ -28,60 +28,60 @@
 							</ul>
 						</header>
 						<div class="content">
-							<span class="image fill" data-position="center"><img src="images/pic01.jpg" alt="" /></span>
+							<span class="image fill" data-position="center"><img src="images/profile.jpg" alt="" /></span>
 						</div>
 					</section>
 
 				<!-- Section -->
 					<section id="first">
 						<header>
-							<h2>나의 20대 이야기</h2>
+							<h2>You've got a friend in me</h2>
 						</header>
 						<div class="content">
-							<p><strong>Lorem ipsum dolor</strong> sit amet consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor amet fermentum. Nullam venenatis erat id vehicula ultrices sed ultricies condimentum. Magna sed etiam consequat, et lorem adipiscing sed nulla. Volutpat nisl et tempus et dolor libero, feugiat magna tempus, sed et lorem adipiscing.</p>
-							<span class="image main"><img src="images/pic02.jpg" alt="" /></span>
+							<p><strong>인간관계</strong> 는 나에게 있어서 항상 우선이 되왔다. 언제나 친구들을 옆에 두고 싶어한 나는 많은 사람들과 조우하고 어울리기를 원하는 한편, 나 스스로의 내면에 상처를 받기 쉬운 예민한 성격또한 존재하여 그 합의점을 찾기가 힘들었다. 어렸을 적에는 그저 나의 문제로만 치부하고 혼자 끙끙 앓았던 적이 많았고, 내 안에서만 문제를 찾으려 했다. 점점 더 많은 사람들과 관계를 이루어 나가면서 그저 나만의 문제가 아니라 내가 다른 이들의 말을 들을 뿐 아니라, 비교적 적극적으로 남들에게 나를 더 내비치면서 내 예민한 성격 한 부분과의 합의점을 찾을 수 있었다.<br><br><br><br><br><br><br><br></p>
+							<span class="image main"><img src="images/recoring.jpg" alt="" /></span>
 						</div>
 					</section>
 
 				<!-- Section -->
 					<section>
 						<header>
-							<h2>Feugiat consequat tempus ultrices</h2>
+							<h2>다 큰 나의 놀이터</h2>
 						</header>
 						<div class="content">
-							<p><strong>Etiam tristique libero</strong> eu nibh porttitor amet fermentum. Nullam venenatis erat id vehicula ultrices sed ultricies condimentum.</p>
-							<ul class="feature-icons">
-								<li class="icon solid fa-laptop">Consequat tempus</li>
-								<li class="icon solid fa-bolt">Etiam adipiscing</li>
-								<li class="icon solid fa-signal">Libero nullam</li>
-								<li class="icon solid fa-cog">Blandit condimentum</li>
-								<li class="icon solid fa-map-marker-alt">Lorem ipsum dolor</li>
-								<li class="icon solid fa-code">Nibh amet venenatis</li>
-							</ul>
-							<p>Vehicula ultrices sed ultricies condimentum. Magna sed etiam consequat, et lorem adipiscing sed nulla. Volutpat nisl et tempus et dolor libero, feugiat magna tempus, sed et lorem adipiscing.</p>
+							<p><strong>내 첫작품의 녹음실</strong>이다. 내 두번째 인생이 시작된 곳인 만큼 크게 사진을 박고 싶었다!</p>
+							<p>내 인생이 어떻게 바뀌었고, 무슨일이 있었는지 털어놓는 공간이다. 내게 있어서 큰 목표는 없었지만 잔잔한 재미를 즐긴다면 그 나름대로 맛이 있지 않을까 :)</p>
 						</div>
 					</section>
 
 				<!-- Section -->
 					<section>
 						<header>
-							<h2>Ultrices erat magna sed condimentum</h2>
+							<h2>나는 누굴까</h2>
 						</header>
 						<div class="content">
-							<p><strong>Integer mollis egestas</strong> nam maximus erat id euismod egestas. Pellentesque sapien ac quam. Lorem ipsum dolor sit nullam.</p>
-
+							<p><strong>어렸을때, </strong>대략 중고등학교때부터 20대 중반까지의 난 항상 주변 사람들이 나를 재밌는 사람으로 생각했으면 하는 바램이 있었다. 선생님이나 친구들을 성대모사해서 웃기는 게 좋았고, 따분한 분위기가 너무나 싫었다. 맨날 유튜브나 만화 캐릭터에 꽂히면 한달 내내 따라하기만 해서 친구들이 처음엔 똑같다고 좋아하다가 점점 괴로워하긴 했다. 항상 주위에 새로운 사람들이나 친구들이 있어야 했고, 누군가와 이야기를 나누고 내가 혼자가 아니라는 자기의식이 그때의 나에게는 너무나도 중요했다. 그래서 이따금씩 혼자가 되면 알 수 없는 불안감에 빠지고 곁에 의지할 사람을 너무나도 찾았다. 그렇게 된 배경에는 나의 자의식에 대한 공포심으로부터 비롯된 게 아닌가 싶다. 자신의 삶에 대해 고민을 가지는 친구들이 나에게는 그저 신기했고, ‘나는 딱히 그런 고민이 없는데..’ 라며 항상 넘겨왔지만 누구보다도 내 자신에 대해 모른다는 사실을 회피하고 싶은 마음에서 나온 혼잣말, 나를 안심시키기 위한 행동이었던 듯 하다. 그러한 것에 대해서 회피하는 것도 곧 취직을 하면 그만두겠지 라는 생각으로 또 다른 회피를 해왔다. 하지만 원하던 공기업에 취직을 하고서도 그 생각은 가시지 않았다. 그럴 때 마다 나는 군대시절 내 맞선임이 해줬던 말이 항상 머리 한켠에 어렴풋이 떠오르곤 했다. ‘사색’이라는 것을 해본 적 있냐는 선임의 질문에 나는 그저 벙찌기만 했다. 선임이 말하기를 자기자신에 대한 고민 때문에 1년 정도를 곁에 누군가 없이 오로지 혼자 다닌 적이 있었고, 이제는 본인에 대한 주관이 확실해졌다라고 했다. 이제서야 말하지만 그때 이해를 못한 건 둘째치고 지금도 이 말은 잘 모르겠다(웃음).</p>
+					<section>
+						<header>
+							<h2>새로운 시작</h2>
+						</header>
+						<div class="content">
+							<p><strong>졸업</strong>을 하고 취준을 하여서 공기업을 다니기를 3년, 나는 반복되는 삶에 적응된 나를 발견하고 기분이 얼떨떨했다. 어느 덧 3년. 누군가에게는 길고, 또 누군가에게는 짧은 시간이지만 보통 연인끼리 기념일도 연수로 챙기지 않나? 나이가 들수록 시간이 더 빠르게 간다고는 하지만 3년이란 시간은 결코 짧게 느껴지지 않았다. 어느덧 서른이 되었고 나름 규칙적인 삶을 가지고 운동도 하면서 안정적인 인생을 살고 있다고는 생각하고 있었지만 어딘가 모르게 부족했다. 핸드폰을 켜 친구들이랑 카톡을 하고 손가락으로는 ‘ㅋㅋㅋㅋㅋㅋㅋ’를 연타하고 있지만 사실 그렇게 막 웃음이 나오지는 않았다. 어딘가 이상했다. 애써 나 스스로의 생각을 멈추기 위해 평소처럼 친구들에게 전화를 돌렸고 끊임없이 얘기를 나누다가 퇴근 후 여가시간을 다 보냈다. 해소된 듯 하지만 아직까지 공허한 마음은 떠나지 않았다. 공허하다는 단어는 꽤나 우울해 보이니, 표현을 바꿔 말하자면 마치 유튜브만 4-5시간 봐서 아무 의미없이 화면만 멍하니 바라보는 그런 기분이었다. 그러기를 약 한달 점심시간, 어린이 채널에서 조연출을 하고 있는 동아리 선배에게 연락이 왔다. 조연 캐릭터 녹음 성우가 빵꾸가 나서 급하게 사람을 찾고있는데, 내가 항상 만화 캐릭터를 따라하던게 생각이 나서 전화를 한 것 이었다. 그 말을 듣는 순간 근래에 느끼지 못했던 설렘이 느껴졌다. 바로 반차를 내고 녹음실에 달려갔고, 녹음실에 들어가고 나오는 순간 이후로 내 인생의 전환점을 맞이했다.</p>
+							<p>첫 연기를 마치고 나서 모두가 조금은 놀란 표정이었다고 한다. 그때의 나는 너무 떨려서 제대로 인사도 못하고 컷이라는 소리에 나와서 허겁지겁 90도로 인사를 하고 나왔다. 다음 날 선배한테 연락이 다시 왔다. 보나마나 다음에 밥 한끼 하자, 술이나 마시자는 둥, 형식적인 얘기를 하겠다는 생각으로 카톡을 봤지만, 다음주에 한번 더 해줄 수 있냐는 소리였다. 그 순간 장난치는 건가 싶었지만 3초만에 바로 달려가겠다는 답장을 보내고 일주일 내내 기다리기만 하였다. 그렇게 매주 녹음에 나가게 되고 자연스레 회사는 그만두게 되었다. 일주일에 한번 뿐이고 알바생 시급을 받는 입장이었지만 나에게는 일처럼 느껴지지 않았다. 그저 내가 어렴풋이 성우 해보고 싶다 라는 꿈에 당첨된 나는 한달 뒤 다니던 회사를 몰래 그만두고, 그 동안 모아온 돈으로 성우학원에 등록하였다. 결정적으로 용기를 준건 같은 시간대에 녹음을 하던 주인공 역할의 성우 선배님이었다. 처음에는 맘에 들지 않은 눈치였지만 회사를 그만두고 학원에 등록한 후부터 항상 나에게 매주 과제를 내주셨다. 같은 단어로 다른 감정 말하기부터 시작해서 얇은 목소리, 할아버지 목소리 내기 등 학원에서는 배울 수 없는 것들을 직접 가르쳐주셨고 그 덕분인지 공채시험 3회만에 같은 방송사는 아니어도 다른 방송사 전문성우에 합격하였다. 전속성우로 2년을 활동한 이후에는 프리랜서가 되어서는 에니메이션뿐만 아니라 안내방송 및 정말 하고 싶었던 게임 캐릭터 배역까지 따내기 위해 작은 배역도 마다 않고 녹음에 참여했다. 인연이 닿아서 인지 첫 녹음 때 만났던 선생님과 같은 게임 녹음에서 만나게 되었고, 진심어린 조언을 많이 듣고 새로운 걸 더 배워 나갔다. 마침 유튜브 채널도 간간히 하고 있던 나는 선생님께 출연을 요청했고, 흔쾌히 승락하셨다. 단순히 목소리만 내는게 아니라 내가 직접 그 캐릭터, 역할이 되어야한다고 하신 선생님은 천생 연기자 아니랄까봐 내 채널에서 혼이 실린 연기를 해주셨고 알고리즘의 혜택을 받아 채널의 성장은 둘째치고 프리랜서로서의 내 삶도 2막을 맞이했다.</p>
+						</div>
+					</section>
 							<!-- Section -->
 								<section>
 									<header>
-										<h3>Erat aliquam</h3>
-										<p>Vehicula ultrices dolor amet ultricies et condimentum. Magna sed etiam consequat, et lorem adipiscing sed dolor sit amet, consectetur amet do eiusmod tempor incididunt  ipsum suspendisse ultrices gravida.</p>
+										<h3>Filmography</h3>
+										<p>대표작</p>
+											<p><strong>릭엔모티(Rick and Morty)</strong><br>ss6~9 - 모티 스미스<br><strong>리그오브레전드</strong><br>요네 / 다리우스</p>
 									</header>
 									<div class="content">
 										<div class="gallery">
 											<a href="images/gallery/fulls/01.jpg" class="landscape"><img src="images/gallery/thumbs/01.jpg" alt="" /></a>
-											<a href="images/gallery/fulls/02.jpg"><img src="images/gallery/thumbs/02.jpg" alt="" /></a>
-											<a href="images/gallery/fulls/03.jpg"><img src="images/gallery/thumbs/03.jpg" alt="" /></a>
-											<a href="images/gallery/fulls/04.jpg" class="landscape"><img src="images/gallery/thumbs/04.jpg" alt="" /></a>
+											<a href="images/gallery/fulls/02.jpg"><img src="images/gallery/thumbs/02.gif" alt="" /></a>
+											<a href="images/gallery/fulls/03.jpg"><img src="images/gallery/thumbs/03.gif" alt="" /></a>
 										</div>
 									</div>
 								</section>
@@ -89,14 +89,15 @@
 							<!-- Section -->
 								<section>
 									<header>
-										<h3>Nisl consequat</h3>
-										<p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam sed facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet mauris. Ut magna finibus nisi nec lacinia ipsum maximus.</p>
+										<h2>다시 발견한 나,<br>다시 본 나</h2>
 									</header>
 									<div class="content">
 										<div class="gallery">
-											<a href="images/gallery/fulls/05.jpg" class="landscape"><img src="images/gallery/thumbs/05.jpg" alt="" /></a>
-											<a href="images/gallery/fulls/06.jpg"><img src="images/gallery/thumbs/06.jpg" alt="" /></a>
-											<a href="images/gallery/fulls/07.jpg"><img src="images/gallery/thumbs/07.jpg" alt="" /></a>
+											<p><strong>10년</strong>, 처음시작과는 앞자리가 다른 40대의 내 삶은 다를 것이 없었다. 다를 것이 없으면 지겹겠다고 생각할 수 있지만 매일매일이 즐거웠다. 유튜브 채널도 상승세를 타서 꽤나 안정적인 수입원이 되었고, 일이 없는 날에는 인터넷 방송도 키면서 항상 즐거움이라는 것이 떠나지 않았다. 내게 있어서 재미라는 것이 가장 큰 원동력이다. 모두가 그렇겠지만 본인 일에 재미가 없을 때도 있지 않은가? 대다수가 굉장히 재미없다는 것에 동의할 것이다. 나는 그런 맥락에서 굉장히 행운아였다. 좋아하는게 일이 된다면 그게 싫어질 것이라는 말들은 뭐.. 아직까지는 잘 모르겠다고 생각됬다. 아직까지 내 일이 너무나 좋았고 쉬는날이면 맨날 내가 나온 것들을 돌려보고, 그만 좀 보라는 얘기는 항상 귀에 달고 살았다. 2년, 3년 지나다보니 같은 과를 나온 친구들도 처음 회사를 계속 다니고 있는 친구는 한명도 없었다. 기계과를 나왔지만 프로그래머가 하고 싶던 친구는 배달의 민족 개발자가 되어있었고, 옷을 좋아하던 친구는 양말사업으로 시작하여서 이제는 어느덧 자리를 잡은 의류사업자가 되어있었다.
+												50, 60 대의 나는 아직까지도 재미가 내 삶의 1순이었다. 유튜브나 강연회, 인생계획서들을 보면 본인들의 삶의 규칙들이 있고, 그것들을 따라하면 꿈을 이룰 수 있다고 한다. 100살 남짓 사는 인생을 반 넘게 살아온 나는 아직도 이 영상들을 보면 2시간 정도 감명받고 까먹는다… 이들을 비하하는게 아니라 정답이 없다는 것이다. 20대의 내가 생각난다. 꿈이 없다고 발버둥치며 무언가를 좇기 위해 아둥바둥 무언가를 하려 했지만 결국에는 그렇게 들어간 회사는 3년다니고 그만두었지 않은가? 꿈이라는 것만 바라보면서 살기보다는 주위를 먼저 보는게 어떨까 싶다. 주위에 재밌는 것이 있다면 즐기고, 짧은 목표를 이뤄가며 나를 조금씩 채워 나가면 그게 모여 꿈이라고 생각한다. 나는 죽기전에 내가 꿈을 이뤘다고 생각하기보다는 꿈을 모아왔고, 맛있는 음식을 공들여서 만들면 맛있게 먹고 음미하듯이 내가 모아온 꿈을 음미하면서 삶을 마무리 짓고 싶다.
+												70대에 들어선 나는 성우직은 간간히 유지하며, 작은 목표를 세웠다. 손자, 손녀들이 아는 것들 모르지 않기! 눈치안보면 살 수 없는 대한민국에서는 할아버지가 되서도 눈치를 봐야한다 껄껄.
+												가상현실에서 10대인척 하면서 속이는 컨텐츠가 꽤나 유행이어서 유튜브도 꽤나 올렸더니, 반응이 나쁘지 않아서 가족 모임 때 꽤나 자랑도 했다. 얼마전에는 인공눈 이식받는다고 했더니만 다들 걱정하길래 몰래 가서 받고 왔다. 덕분에 이렇게 글도 쓰고 얼마나 좋은가. 아직도 나는 100살의 나는 상상이 가지 않는다. 확실한 건 녹음실까지 가는 것도 무리라서 성우는 못하겠지만 유튜브는 계속 하고 싶다. 이 글을 보는 누군가는 연륜이 담기지도 않고 그 흔한 충고도 없는 글이라고 실망하겠지만, 위에서도 말했듯이 나는 사색이란걸 아직도 잘 모른다. 걱정이 많아서 좋은 건 없다. 물론 팡팡 놀라는 말은 아니다. 그저 주위 사람들과 가까운 목표들을 바라보면 마치 경험치 쌓듯이 훌쩍 커있는 나를 볼 수 있을 것이다. 
+											</p>
 										</div>
 									</div>
 								</section>
@@ -104,13 +105,14 @@
 							<!-- Section -->
 								<section>
 									<header>
-										<h3>Lorem gravida</h3>
-										<p>Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aenean ornare velit lacus, ac varius sed enim lorem ullamcorper dolore.  ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis.</p>
+										<h3>김명준 성우</h3>
+										<p>가장 존경하는 선배님이자,<br>성우 인생 멘토님.<br>존경을 담아 한켠에 남깁니다.</p>
+										<p>대표작<br><strong>위베어베어스</strong><br>판다<br><strong>리그오브레전드</strong><br>케인/그림자암살자</p>
 									</header>
 									<div class="content">
 										<div class="gallery">
-											<a href="images/gallery/fulls/08.jpg" class="portrait"><img src="images/gallery/thumbs/08.jpg" alt="" /></a>
-											<a href="images/gallery/fulls/09.jpg" class="portrait"><img src="images/gallery/thumbs/09.jpg" alt="" /></a>
+											<a href="images/gallery/fulls/08.jpg" class="portrait"><img src="images/gallery/thumbs/08.png" alt="" /></a>
+											<a href="images/gallery/fulls/09.jpg" class="portrait"><img src="images/gallery/thumbs/09.png" alt="" /></a>
 											<a href="images/gallery/fulls/10.jpg" class="landscape"><img src="images/gallery/thumbs/10.jpg" alt="" /></a>
 										</div>
 									</div>
@@ -120,18 +122,7 @@
 					</section>
 
 				<!-- Section -->
-					<section>
-						<header>
-							<h2>Duis sed adpiscing veroeros amet</h2>
-						</header>
-						<div class="content">
-							<p><strong>Proin tempus feugiat</strong> sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore.</p>
-							<ul class="actions">
-								<li><a href="#" class="button primary large">Get Started</a></li>
-								<li><a href="#" class="button large">Learn More</a></li>
-							</ul>
-						</div>
-					</section>
+
 
 				<!-- Elements -->
 				<!--
@@ -407,7 +398,7 @@ print 'Sorted in ' + i + ' iterations.';</code></pre>
 							<h2>Get in touch</h2>
 						</header>
 						<div class="content">
-							<p><strong>Auctor commodo</strong> interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis.</p>
+							
 							<form>
 								<div class="fields">
 									<div class="field half">
@@ -429,33 +420,31 @@ print 'Sorted in ' + i + ' iterations.';</code></pre>
 							<ul class="items">
 								<li>
 									<h3>Email</h3>
-									<a href="#">information@untitled.ext</a>
+									<a href="#">sangchu9704@gmail.com</a>
 								</li>
 								<li>
 									<h3>Phone</h3>
-									<a href="#">(000) 000-0000</a>
+									<a href="#">010-3720-1640</a>
 								</li>
 								<li>
 									<h3>Address</h3>
-									<span>1234 Somewhere Road, Nashville, TN 00000</span>
+									<span>서울시 송파구 중대로24 올림픽훼미리타운 110-1004</span>
 								</li>
 								<li>
 									<h3>Elsewhere</h3>
 									<ul class="icons">
-										<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-										<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-										<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-										<li><a href="#" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
-										<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
-										<li><a href="#" class="icon brands fa-codepen"><span class="label">Codepen</span></a></li>
+										
+										<li><a href="https://www.facebook.com/profile.php?id=100003895908568" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+										<li><a href="https://www.instagram.com/sa_wwww/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										
+										<li><a href="https://github.com/sangchu422?tab=repositories" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+									
 									</ul>
 								</li>
 							</ul>
 						</footer>
 					</section>
 
-				<!-- Copyright -->
-					<div class="copyright">&copy; Untitled. All rights reserved. Design: <a href="https://html5up.net">HTML5 UP</a>.</div>
 
 			</div>
 
